29a5b0f217f286d234fa666f0a6d00e597079e0d
collaboratorsMore <1220296015@qq.com>
2017-05-04 22:38:54 +0800

    去掉贪吃蛇

--- mySlider/tools.js
+++
@@ -1,7 +0,0 @@
-function getStyle(obj,styleProp) {
-    if(obj.currentStyle) {
-        return obj.currentStyle[styleProp];
-    }else{
-        return window.getComputedStyle(obj,null)[styleProp];
-    }
-}
